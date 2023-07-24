291b145e0a6d84b842b16a9f2ca1ef04f71ec91c
cjyothi555 <jyothicherukuri555@gmail.com>
2023-07-24 07:27:45 +0530

    added sout origins/main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		System.out.println("first line added");
 		System.out.println("first line added by jyothi");
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("from origins/main");
 	}
 
 }
